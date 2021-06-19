0918b1cf9d94d4b4c464d339bdff57036356ca99
TomDotCom4 <77259800+TomDotCom4@users.noreply.github.com>
2021-06-19 17:15:39 +1000

    Update tommo-popOS.sh

--- tommo-popOS.sh
+++ tommo-popOS.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 LIST_OF_APPS="brave-browser vlc neofetch keepassxc virtualbox flameshot obs-studio gnome-tweaks"
-LIST_OF_REMOVE="firefox gnome-sudoku aisleriot quadrapassel gimp gnome-weather gnome-todo rhythmbox pitivi brasero cheese "
+LIST_OF_REMOVE="firefox gnome-sudoku aisleriot quadrapassel gimp gnome-weather gnome-todo rhythmbox pitivi brasero cheese"
+
+echo " "
+echo "Commencing OS setup..."
+echo " "
+
 apt install apt-transport-https curl
 
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -8,6 +13,8 @@ curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
 
 apt update
+
+apt upgrade
 
 apt install -y $LIST_OF_APPS
 
